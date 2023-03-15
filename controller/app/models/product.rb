80e16ffbb073a37309f6ddb5034c68f6f7a8e67f
KunjJansari <kunj.jansari@bacancy.com>
2023-03-15 16:35:15 +0530

    updating searching functionality

--- controller/app/models/product.rb
+++ controller/app/models/product.rb
@@ -1,16 +1,9 @@
 class Product < ApplicationRecord
   # belongs_to :user
+  # scope :search, ->(search) { where("name like ?", "#{search}%") }
+
   def self.search(search)
-    if search
-      product_name = Product.where("name like ?", "#{search}%")
-      p product_name
-      if product_name
-        Product.where(id: product_name.ids)
-      else
-        @products = Product.all
-      end
-    else
-      @products = Product.all
-    end
+    products = where("name like ?", "#{search}%")
+    products.present? ? products : all
   end
 end
